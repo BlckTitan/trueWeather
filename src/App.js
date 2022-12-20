51d2e4d0ba9a2b1b1bb17ec800bd0d5fa5061978
BlckTitan <dorshman406@gmail.com>
2022-12-20 13:01:51 +0100

    completed the trueWeather webapp by adding a default city weather to display onlLoad and did some styling

--- src/App.js
+++ src/App.js
@@ -3,14 +3,20 @@ import { WEATHER_API_KEY, WEATHER_API_URL } from './api/Api';
 import Search from './components/search/Search';
 import CurrentWeather from './components/currentWeather/CurrentWeather';
 import WeatherForecast from './components/WeatherForecast/WeatherForecast';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 function App() {
 
   const [currentWeatherReport, setCurrentWeatherReport] = useState(null)
   const [weatherForecastReport, setWeatherForecastReport] = useState(null)
-  const defaultLat = 25.3994;
-  const defaultLon = 55.47
+  const [defaultWeatherReport, setDefaultWeatherReport] = useState(null)
+  const [defaultForecastReport, setDefaultForecastReport] = useState(null)
 
+  useEffect(()=>{
+    defaultWeather()
+  }, [])
+
+
+  //user weather fetch
   const handleOnSearchChange = (searchData) => {
     //lon 55.4797
     //lat 25.3994
@@ -29,16 +35,46 @@ function App() {
 
     })
   }
-  console.log(currentWeatherReport)
-  console.log(weatherForecastReport)
 
+  //default  weather fetch
+  const defaultWeather = () => {
+    //lon 55.4797
+    //lat 25.3994
+    
+    const defaultLat = 25.3994;
+    const defaultLon = 55.4797
+  
+    const currentWeatherFetch = fetch(`${WEATHER_API_URL}/weather?lat=${defaultLat}&lon=${defaultLon}&appid=${WEATHER_API_KEY}&units=metric`);
+    const WeatherForecastFetch = fetch(`${WEATHER_API_URL}/forecast?lat=${defaultLat}&lon=${defaultLon}&appid=${WEATHER_API_KEY}&units=metric`)
+    
+    Promise.all([currentWeatherFetch, WeatherForecastFetch])
+    .then(async (response) => {
+      const defaultWeatherResponse  = await response[0].json();
+      const defaultForecastResponse  = await response[1].json();
+
+      setDefaultWeatherReport({city: 'Ajman, AE', ...defaultWeatherResponse});
+      setDefaultForecastReport({city: 'Ajman, AE', ...defaultForecastResponse});
+
+    })
+  }
+
+  //for the rendering, if there is a city that's searched for, give us the result of that city else check the value of the default city and return the value of the default city
+  
   return (
     <div className="container">
       <Search onSearchChange={handleOnSearchChange}/>
-      {currentWeatherReport && <CurrentWeather data={currentWeatherReport}/>}
-      {weatherForecastReport && <WeatherForecast data={weatherForecastReport}/>}
+      {(currentWeatherReport !== null) ? <CurrentWeather data={currentWeatherReport}/> : defaultWeatherReport && <CurrentWeather data={defaultWeatherReport}/>}
+      {(weatherForecastReport !== null) ? <WeatherForecast data={weatherForecastReport}/> : defaultForecastReport && <WeatherForecast data={defaultForecastReport}/>}
     </div>
   );
 }
 
 export default App;
+/*
+  {defaultWeatherReport && <CurrentWeather data={defaultWeatherReport}/>}
+  {defaultForecastReport && <WeatherForecast data={defaultForecastReport}/>}
+  {currentWeatherReport ? <CurrentWeather data={currentWeatherReport}/> : <CurrentWeather data={defaultWeatherReport}/>}
+  {weatherForecastReport ? <WeatherForecast data={weatherForecastReport}/> : <WeatherForecast data={defaultForecastReport}/>}
+  {(currentWeatherReport !== null) ? <CurrentWeather data={currentWeatherReport}/> : <CurrentWeather data={defaultWeatherReport}/>}
+  {(weatherForecastReport !== null) ? <WeatherForecast data={weatherForecastReport}/> : <WeatherForecast data={defaultForecastReport}/>}
+ */
